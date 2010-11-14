fe88785a3e00a575d6a6ae805a388f0b44600bf6
Sam Elkhateeb <same@nanosn.com>
2010-11-13 20:02:58 -0400

    fixed in_development to work even if the os.environ vars are not set

--- src/tic/appengine/utils.py
+++ src/tic/appengine/utils.py
@@ -5,4 +5,5 @@ def in_development():
     """
     Returns True if we are in development
     """
-    return 'Development' in os.environ['SERVER_SOFTWARE']
+    return not os.environ.get('SERVER_SOFTWARE') \
+        or 'Development' in os.environ.get('SERVER_SOFTWARE')
