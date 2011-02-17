58d4677882bf90ff60c8f6eaafd9a6c6b4331f0c
Sam Elkhateeb <sam.elkhateeb@radian6.com>
2011-02-17 09:44:29 -0400

    started working on an example for closure

---
+++ src/example/client/entrypoint.js
@@ -0,0 +1,9 @@
+goog.provide('example.client.entrypoint');
+
+/**
+ * Static method .. could be instance method too..
+ * ie: example.client.entrypoint.prototype.onModuleLoad
+ */
+example.client.entrypoint.onModuleLoad = function(){
+    document.write('sweet');
+}
