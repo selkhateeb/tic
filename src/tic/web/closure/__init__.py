79e786e0a5a640061a80f481ee4be95b4de54006
Sam Elkhateeb <same@nanosn.com>
2011-02-19 11:37:49 -0400

    added command to complie closure templates (soy files) ./cm.py compile_closure_templates

--- src/tic/web/closure/__init__.py
+++ src/tic/web/closure/__init__.py
@@ -28,3 +28,54 @@ def calculate_deps(js_entrypoint_file_path):
     deps = [base] + tree.GetDependencies(namespace)
 
     return [loader.get_relative_path(js_source.GetPath()) for js_source in deps]
+
+def compile_soy_templates():
+    """
+    Compiles the soy files
+    TODO: cannot do this in appengine dev server since os.popen is not defined
+    so a better approach would be to run a deamon that that monitors the file
+    system and generates all needed stuff
+    """
+    import os
+    import shutil
+    import glob
+    logging.info('Scanning for soy template files...')
+    template_files = set()
+    for template_file in loader.locate("*.soy"):
+        template_files.add(template_file)
+
+    if not template_files:
+        logging.info('No templates found.')
+        return
+
+    generated_path = "%sgenerated/templates/" % loader.root_path()
+
+    logging.info('Found %s template(s)' % len(template_files))
+
+    shutil.rmtree(generated_path)
+    os.makedirs(generated_path)
+
+    logging.info('compiling ...')
+    a = os.popen("java -jar /Users/selkhateeb/Development/Projects/tic/tools/closure-templates/SoyToJsSrcCompiler.jar --outputPathFormat %(generated_path)s{INPUT_FILE_NAME_NO_EXT}.js %(options)s %(templates)s"
+                 % {'generated_path': generated_path,
+                 'templates': ' sdfas'.join(template_files),
+                 'options':' '.join([
+                    '--shouldGenerateJsdoc',
+                    '--shouldProvideRequireSoyNamespaces'
+                 ])})
+    if a.close():
+        logging.info('Failed to compile... check error message')
+        return
+
+    logging.info('Generated files:')
+    for fname in os.listdir(generated_path):
+        logging.info('\t%s%s' % (generated_path,fname))
+
+    logging.info('Copying required files...')
+#    for file in glob.glob('/Users/selkhateeb/Development/Projects/tic/tools/closure-templates/*.js'):
+    jsutil_file = '/Users/selkhateeb/Development/Projects/tic/tools/closure-templates/soyutils_usegoog.js'
+    logging.info('\t' + jsutil_file)
+    shutil.copy(jsutil_file, generated_path)
+    logging.info('Done.')
+
+    
---
+++ src/tic/web/closure/console.py
@@ -0,0 +1,30 @@
+from tic.admin.api import IAdminCommandProvider
+from tic.core import Component, implements
+
+class ClosureCommand(Component):
+    implements(IAdminCommandProvider)
+
+    def get_admin_commands(self):
+        """
+        Returns a list of commands to execute
+        @see tic.admin.api.IAdminCommandProvider
+        """
+        
+        #(command, args, help, complete, execute)
+        command = "kijiji"
+        args = None
+        help = """test`ing API."""
+
+        complete = None
+#        execute = self._execute
+
+        return (
+                ("compile_closure_templates", None, "Compiles all closure template files (.soy)", complete, self._compile_closure_templates),
+                )
+
+    def _compile_closure_templates(self):
+        """
+        Runs the closure templates compiler
+        """
+        from tic.web import closure
+        closure.compile_soy_templates()
