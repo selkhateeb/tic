f86d4416b964466e82e13a7d2887c501d0339c28
Sam Elkhateeb <same@nanosn.com>
2011-02-21 07:19:58 -0400

    experimenting to write a code coverage for closure

---
+++ src/tic/tools/coverage.py
@@ -0,0 +1,36 @@
+import logging
+from tic.admin.api import IAdminCommandProvider
+from tic.core import Component, implements
+from tic import loader
+from tic.utils.jsparser import parse
+
+class CoverageCommand(Component):
+    implements(IAdminCommandProvider)
+
+    def get_admin_commands(self):
+        """
+        Returns a list of commands to execute
+        @see tic.admin.api.IAdminCommandProvider
+        """
+
+        #(command, args, help, complete, execute)
+
+        return (
+                ("coverage", None, "Generages js coverage reports", None, self._coverage),
+                )
+
+    def _coverage(self):
+        """
+        TODOC
+        """
+        logging.info('Scanning For Js Files')
+        jsfile = "/Users/selkhateeb/Development/Projects/tic/src/example/client/entrypoint.js"
+        nodes = parse(file(jsfile).read())
+        self._recurse(nodes)
+
+    def _recurse(self, nodes):
+        for node in nodes:
+            print nodes.funDecls
+            print nodes.varDecls
+            source = node.getSource()
+            self._recurse(node)
