09b44cbb878a9d1ca8393802798e704d7acaf956
Sam Elkhateeb <same@nanosn.com>
2011-03-14 20:02:19 -0300

    IBuildTask for compiling google closure

--- src/tic/web/closure/console.py
+++ src/tic/web/closure/console.py
@@ -1,34 +1,38 @@
 from tic.admin.api import IAdminCommandProvider
 from tic.core import Component, implements
-from tic.tools.api import IRunServerTask
+from tic.tools.api import IRunServerTask, IBuildTask
 from tic.web import closure
 
 class ClosureCommand(Component):
-    implements(IAdminCommandProvider, IRunServerTask)
+   implements(IAdminCommandProvider, IRunServerTask, IBuildTask)
 
-    #IAdminCommandProvider implementation
-    def get_admin_commands(self):
-        """
-        Returns a list of commands to execute
-        @see tic.admin.api.IAdminCommandProvider
-        """
-        return (
-                ("compile_closure_templates", None, "Compiles all closure template files (.soy)", None, self._run),
-                    ("compile_closure_js", None, "Compiles all closure js files", None, self._compile_closure),
-                
-                )
+   #IAdminCommandProvider implementation
+   def get_admin_commands(self):
+       """
+       Returns a list of commands to execute
+       @see tic.admin.api.IAdminCommandProvider
+       """
+       return (
+               ("compile_closure_templates", None, "Compiles all closure template files (.soy)", None, self._run),
+                   ("compile_closure_js", None, "Compiles all closure js files", None, self._compile_closure),
+               
+               )
 
     #IRunServerTask
-    def run(self):
-        """
-        Runs the closure templates compiler
-        """
-        self._run()
-        
-    def _run(self):
-        closure.prepare_generated_directory()
-        if closure.compile_soy_templates(): #if we have templates copy the required js files
-            closure.copy_required_js_files()
+   def run(self, build_path=None):
+       """
+       Runs the closure templates compiler
+       """
+       if build_path:
+           self._run()
+           self._compile_closure()
+       else:            
+           self._run()
+       
+   def _run(self):
+       closure.prepare_generated_directory()
+       if closure.compile_soy_templates(): #if we have templates copy the required js files
+           closure.copy_required_js_files()
             
-    def _compile_closure(self):
-        closure.compile_closure_files()
+   def _compile_closure(self):
+       closure.compile_closure_files()
