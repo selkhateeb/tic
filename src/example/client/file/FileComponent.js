e1ee8d927230a0d73fc6a3591b40fda4d1fe4701
Sam Elkhateeb <same@nanosn.com>
2011-12-08 23:58:24 -0700

    finalized style and behaviour of file dnd

--- src/example/client/file/FileComponent.js
+++ src/example/client/file/FileComponent.js
@@ -27,6 +27,13 @@ goog.inherits(example.client.file.FileComponent, goog.ui.Component);
  */
 example.client.file.FileComponent.prototype.fileReader_ = new goog.fs.FileReader();
 
+/**
+ * Array contains all the files added
+ * 
+ * @tyope {Array.<File>}
+ */
+example.client.file.FileComponent.prototype.files_ = [];
+
 example.client.file.FileComponent.prototype.fragment = {
 	DROP : 'd',
 	IMAGES: 'i'
@@ -36,12 +43,10 @@ example.client.file.FileComponent.prototype.fragment = {
  * Overrides the base.createDom method
  */
 example.client.file.FileComponent.prototype.createDom = function() {
-	this.element_ = this.getDomHelper().createElement('div');
-
-	this.element_.innerHTML = examples.client.file.template.root({
+	this.element_ = this.getDomHelper().htmlToDocumentFragment(examples.client.file.template.root({
 		id : this.makeId(this.fragment.DROP),
 		images : this.makeId(this.fragment.IMAGES)
-	});
+	}));
 };
 /**
  * Overrides the base.enterDocument method
@@ -50,27 +55,60 @@ example.client.file.FileComponent.prototype.enterDocument = function() {
 	goog.base(this, 'enterDocument');
 
 	var drop = this.getElementByFragment(this.fragment.DROP);
-	goog.events.listen(drop, goog.events.EventType.DRAGENTER, goog.bind(this.cancel_, this));
-	goog.events.listen(drop, goog.events.EventType.DRAGLEAVE, goog.bind(this.cancel_, this));
+	goog.events.listen(drop, goog.events.EventType.DRAGENTER, goog.bind(this.dropDragEnter_, this), false);
+	goog.events.listen(drop, goog.events.EventType.DRAGLEAVE, goog.bind(this.dropDragLeave_, this));
 	goog.events.listen(drop, goog.events.EventType.DRAGOVER, goog.bind(this.cancel_, this));
 	goog.events.listen(drop, goog.events.EventType.DROP, goog.bind(this.dispatchFileReaderEvent_, this));
+
+	goog.events.listen(this.getDomHelper().getDocument(), goog.events.EventType.DRAGENTER, goog.bind(this.bodyDragEnter_, this));
+	goog.events.listen(this.getDomHelper().getDocument(), goog.events.EventType.DRAGLEAVE, goog.bind(this.bodyDragLeave_, this));
+	goog.events.listen(this.getDomHelper().getDocument(), goog.events.EventType.DRAGOVER, goog.bind(this.cancel_, this));
+	goog.events.listen(this.getDomHelper().getDocument(), goog.events.EventType.DROP, goog.bind(this.bodyDragLeave_, this));
+
+    this.fileReader_.addEventListener(goog.fs.FileReader.EventType.LOAD_END, goog.bind(this.fileLoaded_, this));
+};
+
+example.client.file.FileComponent.prototype.dropDragEnter_ = function(e){
+	e.preventDefault();
+	goog.dom.classes.add(this.getElementByFragment(this.fragment.DROP), 'root_animate');
+	console.log('dropDragEnter_');
+};
+example.client.file.FileComponent.prototype.dropDragLeave_ = function(e){
+	e.preventDefault();
+	goog.dom.classes.remove(this.getElementByFragment(this.fragment.DROP), 'root_animate');
+	console.log('dropDragLeave_');
+};
+
+example.client.file.FileComponent.prototype.bodyDragEnter_ = function(e){
+	e.preventDefault();
+	goog.dom.classes.add(this.getElementByFragment(this.fragment.DROP), 'root_hover');
+	console.log('bodyDragEnter_');
+};
+example.client.file.FileComponent.prototype.bodyDragLeave_ = function(e){
+	// e.stopPropagation();
+	e.preventDefault();
+	if(e.clientX === 0 && e.clientY === 0){
+	goog.dom.classes.remove(this.getElementByFragment(this.fragment.DROP), 'root_hover');
+	}
+	console.log('bodyDragLeave_');
+	console.log(e);
 };
 
 example.client.file.FileComponent.prototype.cancel_ = function(e) {
 	e.stopPropagation();
 	e.preventDefault();
+	console.log('cancel_');
 };
 
 example.client.file.FileComponent.prototype.dispatchFileReaderEvent_ = function(e) {
 	e.stopPropagation();
 	e.preventDefault();
-	console.log(e);
+	
 	var dt = e.getBrowserEvent().dataTransfer;
 	var file = dt.files[0];
 	
 	
 	
-    this.fileReader_.addEventListener(goog.fs.FileReader.EventType.LOAD_END, goog.bind(this.fileLoaded_, this));
     this.fileReader_.readAsDataUrl(file);
 };
 
