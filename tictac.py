1220418fc4bd0252b9fee3cc2061cbfaae21ee11
Sam Elkhateeb <same@nanosn.com>
2012-01-28 20:41:08 -0700

    tictac main application

---
+++ tictac.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import argparse
+from tic.development.tictac import CommandLineApplication, subcommands
+
+parser = argparse.ArgumentParser(
+    epilog="See '%(prog)s COMMAND --help' for more information on a specific command.",
+    )
+
+app = CommandLineApplication(parser=parser)
+
+
+for command in subcommands.__all__:
+    c = getattr(subcommands, command)
+    app.add_command(c)
+
+app.run()
+    
