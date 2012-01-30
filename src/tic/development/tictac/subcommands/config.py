cf7387cd850e86020042d51eea33cc794fccbd96
Sam Elkhateeb <same@nanosn.com>
2012-01-29 23:30:19 -0700

    implements add get and remove config options

--- src/tic/development/tictac/subcommands/config.py
+++ src/tic/development/tictac/subcommands/config.py
@@ -19,8 +19,8 @@ class ConfigCommand(object):
                                                      help='Adds a configuration option')
 
         self.add_parser.add_argument('section')
-        self.add_parser.add_argument('key')
-        self.add_parser.add_argument('value')
+        self.add_parser.add_argument('option')
+        self.add_parser.add_argument('value', nargs='?', default='')
         
         self.add_parser.set_defaults(func=self.add)
         
@@ -28,6 +28,14 @@ class ConfigCommand(object):
         self.remove_parser = self.subparsers.add_parser('remove',
                                                         help='Removes a configuration option')
         self.remove_parser.set_defaults(func=self.remove)
+        self.remove_parser.add_argument('section')
+        self.remove_parser.add_argument('option', nargs='?', default='')
+
+        self.get_parser = self.subparsers.add_parser('get',
+                                                        help='Gets a configuration option')
+        self.get_parser.set_defaults(func=self.get)
+        self.get_parser.add_argument('section')
+        self.get_parser.add_argument('option', nargs='?', default='')
 
     @staticmethod
     def add(args, config):
@@ -35,12 +43,12 @@ class ConfigCommand(object):
         - `args`: args passed
         - `config`: configuration object
         """
-        print 'adding'
-        #todo: verify that the seciton exists 
-        #config.add_section(args.section)
-        config.set(args.section, args.key, args.value)
+
+        if not config.has_section(args.section):
+            config.add_section(args.section)
+        config.set(args.section, args.option, args.value)
         config.write()
-        print args
+        
 
     @staticmethod
     def remove(args, config):
@@ -48,4 +56,20 @@ class ConfigCommand(object):
         - `args`: args passed
         - `config`: configuration object
         """
-        print 'removing'        
+        if args.option:
+            config.remove_option(args.section, args.option)
+        else:
+            config.remove_section(args.section)
+        config.write()
+
+
+    @staticmethod
+    def get(args, config):
+        """
+        - `args`: args passed
+        - `config`: configuration object
+        """
+        if args.option:
+            print config.get(args.section, args.option)
+        else:
+            print '\n'.join(config.options(args.section))
