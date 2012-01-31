d5b6d91cbaa0a38a90b9a64738e4336f9f0ea7fc
Sam Elkhateeb <same@nanosn.com>
2012-01-30 21:39:20 -0700

    adds mkdir for src tests and docs directories

--- src/tic/development/tictac/subcommands/init.py
+++ src/tic/development/tictac/subcommands/init.py
@@ -25,14 +25,23 @@ class InitCommand(object):
             print 'tic project already initialized in %s' % path
             
         except Exception, ex:
-        
+
             config_file = os.path.join(args.dir, config.config_file)
             path, filename = os.path.abspath(config_file).rsplit(os.sep, 1)
         
             os.makedirs(path)
+            project_dir = os.path.abspath(args.dir)
+            InitCommand.mkdir_if_not_exist(os.path.join(project_dir, 'src'))
+            InitCommand.mkdir_if_not_exist(os.path.join(project_dir, 'docs'))
+            InitCommand.mkdir_if_not_exist(os.path.join(project_dir, 'tests'))
             open(config_file, 'w').close()
             print 'Initialized new tic project in %s' % path
         
-        
+    @staticmethod    
+    def mkdir_if_not_exist(dir):
+        """
+        """
+        if not os.path.exists(dir):
+            os.mkdir(dir)
         
         
