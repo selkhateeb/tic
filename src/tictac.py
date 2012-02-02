aba7d9444afb4fe85f756a14f8dd9940ecb473b3
Sam Elkhateeb <same@nanosn.com>
2012-02-01 23:22:21 -0700

    adds local path to sys.path

--- src/tictac.py
+++ src/tictac.py
@@ -2,17 +2,21 @@
 
 
 if __name__ == '__main__':
-
+    import sys
     import logging
     import argparse
     from tic.development.tictac import CommandLineApplication, subcommands, Configuration
+
+    logging.getLogger().setLevel(logging.DEBUG)
 
     parser = argparse.ArgumentParser(
         epilog="See '%(prog)s COMMAND --help' for more information on a specific command.",
         )
 
     config = Configuration(config_file='.tic/config')
-    try:
+#    try:
+    if True:
+        sys.path.insert(1,config.get_project_sources_path())
         app = CommandLineApplication(parser=parser, config=config)
     
         for command in subcommands.__all__:
@@ -21,6 +25,7 @@ if __name__ == '__main__':
         
         app.run()
 
-    except Exception, e:
-        logging.error(e.message)
-
+#    except Exception, e:
+#        logging.error(e.message)
+        #import traceback
+        #traceback.print_exc()
